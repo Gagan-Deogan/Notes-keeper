bb8c976a6008c23011a1e7184930101739185e8b
GagandeepSingh0115 <gs816912@gmail.com>
2021-07-01 17:07:37 +0530

    feat:added-hero-image

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,14 @@
 import { useAuth } from "context";
+import heroImage from "assets/hero_image.png";
 import googleIcon from "assets/google.svg";
 export const Login = () => {
   const { loginUser } = useAuth();
   return (
     <>
-      <div className="w100 dis-flx dir-row jst-ctr">
-        <button className="btn-pry" onClick={loginUser}>
+      <div className="w100 dis-flx dir-col jst-ctr alg-itm">
+        <img src={heroImage} alt="hero-image" className="mrg-t-64" />
+        <h1>Start Taking Notes!</h1>
+        <button className="btn-pry mrg-t-16" onClick={loginUser}>
           <img alt="google-icon" src={googleIcon} />
           <span className="mrg-l-8">Login</span>
         </button>
